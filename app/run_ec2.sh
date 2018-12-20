3beaa242b9dde80a93729b9287fddefb05f99890
AlexPadron <alexp@kensho.com>
2018-12-20 08:08:16 -0500

    z

---
+++ app/run_ec2.sh
@@ -0,0 +1,13 @@
+# Helper script for deploying on ec2
+
+# Start ssh agent and add id
+eval "$(ssh-agent -s)"
+ssh-add ../id_aws
+
+# stop existing server
+killall python3
+echo "Done stopping old server"
+
+# Run new server
+sudo python3 -m pip install -r requirements.txt
+sudo sh scripts/deploy.sh & bg
